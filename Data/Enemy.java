b0ba112e6482cce6de26a8f1a43751f02f6e45d1
Ahmed Ahres <ahres.ahmed@yahoo.fr>
2017-02-15 14:37:34 +0100

    Added A.I for enemies to detect corners

--- Data/Enemy.java
+++ Data/Enemy.java
@@ -1,6 +1,7 @@
 package Data;
 
 import org.newdawn.slick.opengl.Texture;
+
 import static helpers.Clock.*;
 
 import java.util.ArrayList;
@@ -9,7 +10,7 @@ import static Data.TileGrid.*;
 import static helpers.Artist.*;
 
 public class Enemy {
-    private int width, height, health;
+    private int width, height, health, currentCheckpoint;
     private float speed, x, y;
     private Tile startTile;
     private Texture texture;
@@ -39,6 +40,41 @@ public class Enemy {
         //y direction
         this.directions[1] = 0;
         directions = FindNextD(startTile);
+        this.currentCheckpoint = 0;
+        PopulateCheckpointList();
+    }
+    
+    private boolean CheckpointReached() {
+        boolean reached = false;
+        Tile t = checkpoints.get(currentCheckpoint).getTile();
+        if(x > t.getX() - 3 && x < t.getX() + 3 && y > t.getY() - 3 && y < t.getY() + 3) {
+            //Check if position reached tile within variance of 3 (which is an arbitrary number)
+            reached = true;
+            x = t.getX();
+            y = t.getY();
+        }
+        
+        return reached;
+    }
+    
+    //enemy pathfinding
+    private void PopulateCheckpointList(){
+        checkpoints.add(FindNextC(startTile, directions = FindNextD(startTile)));
+        
+        int counter = 0;
+        boolean cont = true;
+        while(cont) {
+            int[] currentD = FindNextD(checkpoints.get(counter).getTile());
+            //Check fi a next direction/checkpoints exists, end after 20 checkpoints (arbitrary)
+            if(currentD[0] == 2 || counter == 20){
+                cont = false; //cannot continue anymore because it refers to no directions found
+            } else {
+                checkpoints.add(FindNextC(checkpoints.get(counter).getTile(),
+                                          directions = FindNextD(checkpoints.get(counter).getTile())));
+            }
+            counter++;
+        }
+        
     }
     public void Draw(){
         DrawQuadTex(texture, x, y, width, height);
@@ -48,9 +84,36 @@ public class Enemy {
         if(first) first = false;
         
         else {
-            x += Delta() * directions[0];
-            y += Delta() * directions[1];
+            if(CheckpointReached()) {
+                currentCheckpoint++;
+            } else { //move them
+                x += Delta() * checkpoints.get(currentCheckpoint).getxDirection() * speed;
+                y += Delta() * checkpoints.get(currentCheckpoint).getyDirection() * speed;
+            }
+            
         }
+    }
+    //Finding the checkpoint i.e corner to stop
+    private CheckPoint FindNextC(Tile s, int[] dir){
+        Tile next = null;
+        CheckPoint c = null;
+        //boolean to decide if next checkpoint is found
+        boolean found = false;
+        int counter = 1;
+        
+        while(!found){
+            
+            if(s.getType() != grid.GetTile(s.getXPlace() + dir[0] * counter, s.getYPlace() + dir[1] * counter).getType()){
+                found = true;
+                counter -= 1;
+                next = grid.GetTile(s.getXPlace() + dir[0] * counter, s.getYPlace() + dir[1] * counter);
+            }
+            counter++;
+            
+        }
+        c = new CheckPoint(next, dir[0], dir[1]);
+        
+        return c;
     }
     
     private int[] FindNextD(Tile start){ //Find Next Direction
@@ -64,15 +127,17 @@ public class Enemy {
             dir[0] = 0;
             dir[1] = -1;
         } else if (start.getType() == r.getType()){
-            dir[0] = 0;
-            dir[1] = 1;
-        }  else if (start.getType() == d.getType()){
             dir[0] = 1;
             dir[1] = 0;
-        } else if (start.getType() == l.getType()){
+        }  else if (start.getType() == d.getType()){
             dir[0] = 0;
-            dir[1] = -1;
+            dir[1] = 1;
+        } else if (start.getType() == l.getType()){
+            dir[0] = -1;
+            dir[1] = 0;
         } else {
+            dir[0] = 2;
+            dir[1] = 2;
             System.out.println("NO DIRECTIONS FOUND");
         }
         
